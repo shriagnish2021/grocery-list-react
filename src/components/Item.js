91cfb08485052fe283ea0c9fae20bc026eaed975
shriagnish2021 <shriagnish2021@gmail.com>
2021-07-23 13:24:50 +0530

    edit on enter key added

--- src/components/Item.js
+++ src/components/Item.js
@@ -20,8 +20,18 @@ const Item = ({ item,onDelete,onEdit,id }) => {
             edit:"true",
         })
     }
-    return (
-    <tr contentEditable={itemInput.edit} onInput={onInput} className={(itemInput.edit === 'true'? 'edit':'')} onBlur={() => {
+    const onKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            setItem({
+                itemName:item.itemName,
+                units:item.units,
+                price:item.price,
+                edit:"false",
+            })
+            onEdit(itemInput,id)
+        }
+    }
+    const onBlur = () => {
         setItem({
             itemName:item.itemName,
             units:item.units,
@@ -29,7 +39,10 @@ const Item = ({ item,onDelete,onEdit,id }) => {
             edit:"false",
         })
             onEdit(itemInput,id)
-        }} suppressContentEditableWarning>
+    }
+            
+    return (
+        <tr contentEditable={itemInput.edit} onKeyPress={onKeyPress} onInput={onInput} className={(itemInput.edit === 'true'? 'edit':'')} onBlur={onBlur} suppressContentEditableWarning>
         <td contentEditable="false"><span title="delete item" style={{display:'inline'}}><FaTimes style={{color:'red', cursor:'pointer'}} onClick={() => onDelete(itemInput.itemName)}/></span>&nbsp;&nbsp;<span title="Click and edit your item" style={{display:'inline'}}><FaPenSquare style={{cursor:'pointer'}} onClick={(itemInput) => {
                 setItem({
                     itemName:item.itemName,
